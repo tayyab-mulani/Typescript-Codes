1e846372551a24b97faa20da494313d4811be222
Tayyab Mulani <114877668+tayyab-mulani@users.noreply.github.com>
2023-03-17 19:44:27 +0530

    Add files via upload

---
+++ Area.ts
@@ -0,0 +1,13 @@
+function Area(Radius : number) : void
+{
+    var PI : number = 3.14
+    var iArea : number = 0
+
+    iArea = PI * Radius * Radius
+
+    console.log("Area of Circle is  = "+iArea)
+}
+
+var iRad : number = 5 
+
+Area(iRad)
